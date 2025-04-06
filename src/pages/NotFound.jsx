2c66a9806ee3b3bd8147ce1d96585f76d5dc8451
Ravi-Rathod143 <ravikantrathod143@gmail.com>
2025-04-06 12:23:28 +0530

    changes commit

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,9 +6,9 @@ const NotFound = () => {
   return (
     <div className="not-found-wrapper">
       <div className="not-found-container">
-        <h1>404</h1>
-        <h2>Product Not Found!</h2>
-        <p>Sorry, the product you're looking for doesn't exist or has been removed.</p>
+        <h1>404 - Page Not Found</h1>
+        {/* <h2>Product Not Found!</h2> */}
+        <p>The page you are looking for does not exist.</p>
         <Link to="/" className="back-home-btn">
           Back to Home
         </Link>
